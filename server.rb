98e9b45c0c8d53b25fb10dc7778dd3fa2a0b6784
Alireza Andersen <alireza.andersen@gmail.com>
2016-01-04 15:34:52 -0700

    iteration_0 with Hello World

--- server.rb
+++ server.rb
@@ -1,3 +1,16 @@
 require 'socket'
+
+class Server
+
 tcp_server = TCPServer.new(9292)
 client = tcp_server.accept
+
+# puts "Ready for a request"
+request_lines = []
+while line = client.gets and !line.chomp.empty?
+  request_lines << line.chomp
+end
+ # puts "Got this request:"
+puts "Hello World #{request_lines.count}"
+
+end
