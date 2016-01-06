9334f66c4d6dcd5d60e15e60a39f30edf92c4d7e
Alireza Andersen <alireza.andersen@gmail.com>
2016-01-05 17:55:20 -0700

    Partial Iteration_2 with Support Path and Hello counter

--- lib/server.rb
+++ lib/server.rb
@@ -1,36 +1,60 @@
-require 'socket'
-
 tcp_server = TCPServer.new(9292)
 client = tcp_server.accept
+
 counter = 0
 
-puts "Ready for a request"
-request_lines = []
-while line = client.gets and !line.chomp.empty?
-  request_lines << line.chomp
-end
+loop do
+  puts "Ready for a request"
+  request_lines = []
+  while line = client.gets and !line.chomp.empty?
+    request_lines << line.chomp
+  end
   counter +=1
 
-puts "Got this request:"
-puts request_lines.inspect
-print "\n"
+  puts "Got this request:"
+  puts request_lines.inspect
+  print "\n"
 
-d = {}	#hash containing request diagnostic info
+  d = {}	#hash containing request diagnostic info
 
-#put request into hash 'd'
-request_lines.each.with_index{ |req,index|
-	#print index, " ", req, "\n"
-	if index == 0
-		(d['Verb'], d['Path'], d['Protocol']) = req.split
-	else
-		line_elements = req.split(':')
-		d[line_elements[0].strip] = line_elements[1].strip
-	end
-}
-print "\n*********** DEBUG INFO ************\n"
+  #put request into hash 'd'
+  request_lines.each.with_index{ |req,index|
+    #print index, " ", req, "\n"
+    if index == 0
+      (d['Verb'], d['Path'], d['Protocol']) = req.split
+    else
+      line_elements = req.split(':')
+      d[line_elements[0].strip] = line_elements[1].strip
+    end
+  }
 
-d.each{|key,val|
-	puts "#{key}: #{val}"
-}
+  #process request
+  case d['Path']
+  when '/'
+    puts '/ detected'
+    response(client,'/ detected')
+  when '/hello'
+    puts '/hello detected'
+    response(client, "/hello #{counter}")
+  when '/datetime'
+    puts '/datetime detected'
+    response(client, Time.now.strftime('%a, %e %b %Y %H:%M:%S %z'))
+  when '/shutdown'
+    puts '/shutdown detected'
+    response(client, '/shutdown detected')
+    exit
+  else
+    puts "#{d['Path']} is an unknown command"
+    response(client, "unknown command, #{d['Path']} detected",STATUS_NOTFOUND)
+  end
 
+
+  #output diagnostic info
+  print "\n*********** DEBUG INFO ************\n"
+
+  d.each{|key,val|
+    puts "#{key}: #{val}"
+  }
+
+end
 client.close
