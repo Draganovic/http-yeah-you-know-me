0b5006e3005ac2acc81d68b563cb808ea3446fbf
Alireza Andersen <alireza.andersen@gmail.com>
2016-01-05 17:51:44 -0700

    Refactored Iteration 1 without hello

--- lib/server.rb
+++ lib/server.rb
@@ -1,34 +1,36 @@
 require 'socket'
-require 'hurley'
 
+tcp_server = TCPServer.new(9292)
+client = tcp_server.accept
+counter = 0
 
-class Server
-  tcp_server = TCPServer.new(9292)
-  client = tcp_server.accept
-
-
-  request_lines = []
-  while line = client.gets and !line.chomp.empty?
-    request_lines << line.chomp
-    puts "Hello World #{request_lines.count}"
-  end
-
-
-  puts request_lines.inspect
-
-  puts "Sending response."
-  response = "<pre>" + "HELLO WORLD" + "#{request_lines.count}" + "</pre>"
-  output = "<html><head></head><body>#{response}</body></html>"
-  headers = ["http/1.1 200 ok",
-            "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-            "server: ruby",
-            "content-type: text/html; charset=iso-8859-1",
-            "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-  client.puts headers
-  client.puts output
-
-  puts ["Wrote this response:", headers, output].join("\n")
-  client.close
-  puts "\nResponse complete, exiting."
-
+puts "Ready for a request"
+request_lines = []
+while line = client.gets and !line.chomp.empty?
+  request_lines << line.chomp
 end
+  counter +=1
+
+puts "Got this request:"
+puts request_lines.inspect
+print "\n"
+
+d = {}	#hash containing request diagnostic info
+
+#put request into hash 'd'
+request_lines.each.with_index{ |req,index|
+	#print index, " ", req, "\n"
+	if index == 0
+		(d['Verb'], d['Path'], d['Protocol']) = req.split
+	else
+		line_elements = req.split(':')
+		d[line_elements[0].strip] = line_elements[1].strip
+	end
+}
+print "\n*********** DEBUG INFO ************\n"
+
+d.each{|key,val|
+	puts "#{key}: #{val}"
+}
+
+client.close
