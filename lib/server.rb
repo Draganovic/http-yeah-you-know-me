fa625f0a8a0e126e64bc66e1242e6b9a5c13908d
Alireza Andersen <alireza.andersen@gmail.com>
2016-01-06 13:28:49 -0700

    Constants with Hurley Test first pass

--- lib/server.rb
+++ lib/server.rb
@@ -1,6 +1,15 @@
 require 'socket'
 
-def response(client,msg,status=nil)
+#Constants
+STATUS_OK = '200 OK'
+STATUS_MOVED = '301 Moved Permanently'
+STATUS_UNAUTH = '401 Unauthorized'
+STATUS_FORB = '403 Forbidden'
+STATUS_NOTFOUND = '404 Not Found'
+STATUS_ERROR = '500 Internal Server Error'
+
+
+def response(client,msg,status=STATUS_OK)
 time = Time.now.strftime('%I:%M %p on %A, %B %e, %Y')
 # puts "Sending response."
 response = "<pre>" + msg.to_s + "</pre>"
@@ -15,59 +24,62 @@ client.puts output
 
 end
 
-tcp_server = TCPServer.new(9292)
-client = tcp_server.accept
-counter = 0
-loop do
-puts "Ready for a request"
-request_lines = []
-while line = client.gets and !line.chomp.empty?
-  request_lines << line.chomp
+if __FILE__ == $0
+  tcp_server = TCPServer.new(9292)
+  client = tcp_server.accept
+  counter = 0
+  loop do
+  puts "Ready for a request"
+  request_lines = []
+  while line = client.gets and !line.chomp.empty?
+    request_lines << line.chomp
+  end
+    counter +=1
+
+  puts "******************Got this request:*******************"
+
+  print "\n"
+
+  d = {}	#hash containing request diagnostic info
+
+  #put request into hash 'd'
+  request_lines.each.with_index{ |req,index|
+  	#print index, " ", req, "\n"
+  	if index == 0
+  		(d['Verb'], d['Path'], d['Protocol']) = req.split
+  	else
+  		line_elements = req.split(':')
+  		d[line_elements[0].strip] = line_elements[1].strip
+  	end
+  }
+  #output diagnostic info
+  print "\n*********** DEBUG INFO ************\n"
+
+  d.each{|key,val|
+  	puts "#{key}: #{val}"
+  }
+
+  #process request
+  case d['Path']
+  	when '/'
+  		puts '/ detected'
+  		response(client, d.collect{ |k,v| "#{k}: #{v}" }.join("\n"))
+  	when '/hello'
+  		puts '/hello detected'
+  		response(client, "/hello #{counter}")
+  	when '/datetime'
+  		puts '/datetime detected'
+  		response(client, Time.now.strftime('%I:%M %p on %A, %B %e, %Y'))
+  	when '/shutdown'
+  		puts '/shutdown detected'
+  		response(client, "/Total Requests : #{counter}")
+      exit
+  	else
+  		puts "#{d['Path']} is an unknown command"
+  		response(client, "unknown command, #{d['Path']} detected",STATUS_NOTFOUND)
+  end
+
+  end
+  client.close
+
 end
-  counter +=1
-
-puts "******************Got this request:*******************"
-
-print "\n"
-
-d = {}	#hash containing request diagnostic info
-
-#put request into hash 'd'
-request_lines.each.with_index{ |req,index|
-	#print index, " ", req, "\n"
-	if index == 0
-		(d['Verb'], d['Path'], d['Protocol']) = req.split
-	else
-		line_elements = req.split(':')
-		d[line_elements[0].strip] = line_elements[1].strip
-	end
-}
-#output diagnostic info
-print "\n*********** DEBUG INFO ************\n"
-
-d.each{|key,val|
-	puts "#{key}: #{val}"
-}
-
-#process request
-case d['Path']
-	when '/'
-		puts '/ detected'
-		response(client, d.collect{ |k,v| "#{k}: #{v}" }.join("\n"))
-	when '/hello'
-		puts '/hello detected'
-		response(client, "/hello #{counter}")
-	when '/datetime'
-		puts '/datetime detected'
-		response(client, Time.now.strftime('%I:%M %p on %A, %B %e, %Y'))
-	when '/shutdown'
-		puts '/shutdown detected'
-		response(client, "/Total Requests : #{counter}")
-    exit
-	else
-		puts "#{d['Path']} is an unknown command"
-		response(client, "unknown command, #{d['Path']} detected",nil)
-end
-
-end
-client.close
--- test/server_test.rb
+++ test/server_test.rb
@@ -1,13 +1,37 @@
-require 'server'
-require 'minitest/pride'
+# require 'server'
+# require 'minitest/pride'
+# require 'minitest/autorun'
+#
+#
+# class ServerTest < Minitest::Test
+#
+#   def test_it_is_a_server_object
+#     s = Server.new
+#     assert s
+#   end
+#
+# end
 require 'minitest/autorun'
+require 'minitest/pride'
+require 'hurley'
+require 'socket'
+require './lib/server'
+require 'hurley/addressable'
 
 
 class ServerTest < Minitest::Test
 
-  def test_it_is_a_server_object
-    s = Server.new
-    assert s
-  end
+ attr_reader :client
+
+ def setup
+   @client = Hurley::Client.new "http://127.0.0.1:9292"
+ end
+
+ def test_response
+   response = client.get("http://127.0.0.1:9292")
+   assert response.success?
+   assert_equal "127.0.0.1", client.host
+   assert_equal 9292, client.port
+ end
 
 end
