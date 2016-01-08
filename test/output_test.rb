bd17a36b66108613dc8bfa61393b0bfd3786fcc1
Alireza Andersen <alireza.andersen@gmail.com>
2016-01-07 21:09:21 -0700

    working files

--- test/output_test.rb
+++ test/output_test.rb
@@ -7,3 +7,15 @@ require './lib/processor'
 require './lib/responder'
 require './lib/constants'
 require 'hurley/addressable'
+
+
+class OutputTest < Minitest::Test
+
+
+  def test_validate_time_with_proper_format
+
+    assert_equal Time.now.strftime('%I:%M %p on %A, %B %e, %Y'))
+  end
+
+  def test_hello_counter
+end
